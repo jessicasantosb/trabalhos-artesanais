b3467791c29b2bfcc29ec8b9a599a4dfcc70a905
jessicasantosb <jessicasantosb@outlook.com>
2024-06-06 15:06:48 -0300

    Style: create a skeleton to home cards images

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CiImageOn } from 'react-icons/ci';
 
 interface CardProps {
   id: string;
@@ -17,11 +19,33 @@ export default function HomeCard({
   client,
   price,
 }: CardProps) {
+  const [skeleton, setSkeleton] = useState<boolean>(true);
+
+  const handleSkeleton = () => {
+    setSkeleton(false);
+  };
   return (
     <>
       <Link to={`project/${id}`}>
         <div className='h-64 flex flex-col justify-between shadow-md shadow-geraldine rounded-xl'>
-          <img src={image} alt={title} className='pb-2 h-28 object-cover' />
+          <div className='flex'>
+            <div
+              className={`center h-full min-w-full ${
+                !skeleton && 'hidden'
+              }`}
+            >
+              <CiImageOn size={32} className='text-geraldine' />
+            </div>
+            <img
+              src={image}
+              alt={title}
+              className={`w-full mb-2 h-28 object-cover transition-all opacity-1 ${
+                skeleton && 'opacity-0'
+              }`}
+              onLoad={handleSkeleton}
+            />
+          </div>
+
           <div>
             <h4 className='px-2 uppercase font-bold tracking-tight break-words line-clamp-2'>
               {title}
