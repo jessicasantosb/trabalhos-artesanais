95323b30f8c1117fcdbbf1c72104cdaf679acb78
jessicasantosb <jessicasantosb@outlook.com>
2024-06-09 19:54:08 -0300

    Feat: create a skeleton and a slide for project images

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,6 +1,12 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import { IoIosColorPalette, IoIosPricetags } from 'react-icons/io';
+import { useEffect, useRef, useState } from 'react';
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoIosColorPalette,
+  IoIosImages,
+  IoIosPricetags,
+} from 'react-icons/io';
 import { MdDateRange, MdPhotoSizeSelectLarge } from 'react-icons/md';
 import { useNavigate, useParams } from 'react-router-dom';
 import { db } from '../services/firebaseConnection';
@@ -28,8 +34,32 @@ interface ProjectProps {
 
 export default function Project() {
   const [project, setProject] = useState<ProjectProps>();
+  const [skeleton, setSkeleton] = useState<boolean>(true);
+  const [imageIsActive, setImageIsActive] = useState<number>(0);
+  const [imagePosition, setImagePosition] = useState<number>(0);
+  const contentRef = useRef<HTMLDivElement>(null);
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const handleSkeleton = () => {
+    setSkeleton(false);
+  };
+
+  const previewImage = () => {
+    if (imageIsActive > 0) setImageIsActive(imageIsActive - 1);
+  };
+
+  const nextImage = () => {
+    if (project && imageIsActive < project.images.length - 1)
+      setImageIsActive(imageIsActive + 1);
+  };
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const { width } = contentRef.current?.getBoundingClientRect();
+      setImagePosition(-(width * imageIsActive));
+    }
+  }, [imageIsActive]);
 
   useEffect(() => {
     const loadProject = async () => {
@@ -65,17 +95,46 @@ export default function Project() {
   }, [id]);
 
   return (
-    <section className='min-h-screen container m-auto'>
+    <section className='container m-auto p-4'>
       <div className='relative my-10'>
-        <h1 className='text-3xl before:content-[""] before:block before:w-6 before:h-6 before:absolute before:-bottom-3 before:-left-3 before:bg-geraldine before:-z-10'>
+        <h1 className='text-3xl before:w-6 before:h-6 before:absolute before:-bottom-3 before:-left-3 before:bg-geraldine before:-z-10'>
           {project?.title}
         </h1>
       </div>
 
-      <div className='flex'>
-        {project?.images.map((image) => {
-          return <img key={image.name} src={image.url} alt={image.name} />;
-        })}
+      <div className='relative h-72 overflow-hidden border-blue border-4 shadow-sm shadow-geraldine'>
+        <div
+          ref={contentRef}
+          className='h-full flex transition-opacity'
+          style={{ transform: `translateX(${imagePosition}px)` }}
+        >
+          <div className={`center h-full min-w-full ${!skeleton && 'hidden'}`}>
+            <IoIosImages size={32} className='text-geraldine' />
+          </div>
+          {project?.images.map((image) => {
+            return (
+              <img
+                key={image.name}
+                src={image.url}
+                alt={image.name}
+                onLoad={handleSkeleton}
+                className={`min-w-full object-cover opacity-1 ${
+                  skeleton && 'opacity-0'
+                }`}
+              />
+            );
+          })}
+        </div>
+        {project && project?.images.length > 1 && (
+          <div className='[&>*]:absolute [&>*]:top-[45%] [&>*]:bg-blue text-white'>
+            <button onClick={previewImage} className='group'>
+              <IoIosArrowBack size={32} className='group-active:scale-75' />
+            </button>
+            <button onClick={nextImage} className='group right-0'>
+              <IoIosArrowForward size={32} className='group-active:scale-75' />
+            </button>
+          </div>
+        )}
       </div>
 
       <p className='pt-6 '>
@@ -86,7 +145,7 @@ export default function Project() {
       <div className='my-6 center flex-col items-start gap-2 [&>*]:center [&>*]:gap-2'>
         <p>
           <IoIosColorPalette size={20} className='text-geraldine' />
-          {project?.color}
+          {project?.color.toLowerCase()}
         </p>
         <p>
           <MdDateRange size={20} className='text-geraldine' />
