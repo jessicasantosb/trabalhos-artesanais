2e19bec52b3a55a1027a8b96f9986e7dc000aefb
jessicasantosb <jessicasantosb@outlook.com>
2024-06-10 08:01:06 -0300

    Style: improve home panel layout

--- src/components/HomePanel.tsx
+++ src/components/HomePanel.tsx
@@ -1,9 +1,6 @@
-import {
-  IoMdArrowDropdown,
-  IoMdArrowDropup,
-  IoMdCreate,
-  IoMdSearch,
-} from 'react-icons/io';
+import { useState } from 'react';
+import { IoMdCreate, IoMdSearch } from 'react-icons/io';
+import { MdOutlineFilterList } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 interface HomePanelProps {
@@ -23,53 +20,72 @@ export default function HomePanel({
   setInputColor,
   handleSearchColor,
 }: HomePanelProps) {
+  const [openedFilter, setOpenedFilter] = useState(false);
+
+  const handleFilterButton = () => {
+    if (openedFilter) return setOpenedFilter(false);
+    setOpenedFilter(true);
+  };
+
   return (
     <section className='my-6 p-4 shadow-md shadow-geraldine rounded-xl center flex-col gap-4'>
-      <Link to={'create'} className='center gap-2 underline'>
-        Criar um novo trabalho
-        <IoMdCreate />
-      </Link>
+      <div className='w-full center flex-row-reverse flex-wrap justify-between gap-2'>
+        <Link to={'create'} className='center gap-2 md:text-lg hover:text-blue'>
+          Criar um novo trabalho
+          <IoMdCreate size={18} />
+        </Link>
 
-      <h3 className='py-4 text-xl flex items-end gap-2'>
-        Filtros <IoMdArrowDropdown />
-        <IoMdArrowDropup />
-      </h3>
-
-      <div className='w-full max-w-96 center'>
-        <input
-          type='text'
-          placeholder='pesquise pelo título'
-          value={inputTitle}
-          onChange={(e) => setInputTitle(e.target.value)}
-          className='w-full'
-        />
-        <button onClick={handleSearchTitle}>
-          <IoMdSearch size={20} />
+        <button
+          onClick={handleFilterButton}
+          className='md:text-lg center gap-2 hover:text-blue'
+        >
+          Filtros
+          <MdOutlineFilterList size={18} />
         </button>
       </div>
 
-      <div className='w-full max-w-96 center'>
-        <input
-          type='text'
-          placeholder='pesquise pela cor'
-          value={inputColor}
-          onChange={(e) => setInputColor(e.target.value)}
-          className='w-full'
-        />
-        <button onClick={handleSearchColor}>
-          <IoMdSearch size={20} />
-        </button>
-      </div>
+      {openedFilter && (
+        <div className='w-full my-4 center flex-col gap-6'>
+          <div className='center flex-row flex-wrap gap-6'>
+            <div className='filterBtnDiv'>
+              <input
+                type='text'
+                placeholder='pesquise pelo título'
+                value={inputTitle}
+                onChange={(e) => setInputTitle(e.target.value)}
+                className='filterBtnInput'
+              />
+              <button onClick={handleSearchTitle} className='filterBtn'>
+                <IoMdSearch size={20} />
+              </button>
+            </div>
 
-      <div className='w-full center justify-between'>
-        <select>
-          <option value='ano' disabled>
-            ano
-          </option>
-          <option value='2024'> 2024</option>
-        </select>
-        <input type='range' name='price' />
-      </div>
+            <div className='filterBtnDiv'>
+              <input
+                type='text'
+                placeholder='pesquise pela cor'
+                value={inputColor}
+                onChange={(e) => setInputColor(e.target.value)}
+                className='filterBtnInput'
+              />
+              <button onClick={handleSearchColor} className='filterBtn'>
+                <IoMdSearch size={20} />
+              </button>
+            </div>
+          </div>
+
+          <div className='center flex-wrap gap-6'>
+            <select>
+              <option value='ano' disabled>
+                ano
+              </option>
+              <option value='2024'> 2024</option>
+            </select>
+
+            <input type='range' name='price' />
+          </div>
+        </div>
+      )}
     </section>
   );
 }
