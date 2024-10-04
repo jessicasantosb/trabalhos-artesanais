204a2c574ffbe378e1e16d7ddf44c56f75eafe7b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-04 14:40:08 -0300

    test: create head test

---
+++ src/tests/components/Head.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Head } from '../../components';
+
+describe('Components - Head', () => {
+  it('should render Head', () => {
+    render(
+      <MemoryRouter>
+        <Head title='Head title' description='Head description' />
+      </MemoryRouter>
+    );
+  });
+});
