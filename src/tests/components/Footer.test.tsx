9c2e5eac5f89c320b729733cf3f47b1954a611bd
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-04 14:39:35 -0300

    test: create footer test

---
+++ src/tests/components/Footer.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Footer } from '../../components';
+
+describe('Components - Footer', () => {
+  it('should render Footer', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    expect(getByText('ALGUMAS IDEIAS')).toBeInTheDocument();
+    expect(getByText('SOBRE NÓS')).toBeInTheDocument();
+    expect(
+      getByText('© 2024 - Alguns direitos reservados')
+    ).toBeInTheDocument();
+  });
+});
