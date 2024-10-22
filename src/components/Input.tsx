511c4400ae81f09d06f5b4ae1c43557afd379192
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-22 16:33:59 -0300

    fix: create input div to add labels

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -26,25 +26,27 @@ export function Input({
 
   return (
     <div className='w-full flex flex-col'>
-      <label className='group flex shadow shadow-geraldine  focus-within:shadow-blue focus-within:outline outline-blue outline-1  bg-white [&>*]:bg-white'>
+      <label className='pt-2'>
         {label}
-        <input
-          placeholder={placeholder}
-          defaultValue={defaultValue}
-          type={inputType}
-          {...register(name, rules)}
-          id={name}
-          className='w-full h-10 p-2 outline-none'
-        />
-        {isPassword && (
-          <button
-            type='button'
-            className='p-2 outline-none'
-            onClick={handleInputType}
-          >
-            {inputType === 'password' ? <IoEye /> : <IoEyeOff />}
-          </button>
-        )}
+        <div className='group flex shadow shadow-geraldine  focus-within:shadow-blue focus-within:outline outline-blue outline-1  bg-white [&>*]:bg-white'>
+          <input
+            placeholder={placeholder}
+            defaultValue={defaultValue}
+            type={inputType}
+            {...register(name, rules)}
+            id={name}
+            className='w-full h-10 p-2 outline-none'
+          />
+          {isPassword && (
+            <button
+              type='button'
+              className='p-2 outline-none'
+              onClick={handleInputType}
+            >
+              {inputType === 'password' ? <IoEye /> : <IoEyeOff />}
+            </button>
+          )}
+        </div>
       </label>
 
       {error && <p className='my-1 text-red text-sm'>{error}</p>}
