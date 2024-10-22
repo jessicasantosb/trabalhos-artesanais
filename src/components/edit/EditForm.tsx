ed220f75abd727ce5ba21e2acd178bf8f5140282
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-22 15:59:59 -0300

    style: organize inputs layout

--- src/components/edit/EditForm.tsx
+++ src/components/edit/EditForm.tsx
@@ -19,81 +19,90 @@ export function EditForm({
   return (
     <form
       onSubmit={handleSubmit(handleEditProject)}
-      className='my-6 center flex-col items-start gap-2 [&>*]:center [&>*]:gap-2'
+      className='my-6 flex flex-col items-center gap-2'
     >
-      <div>
-        <IoSquare size={20} className='text-geraldine' />
-        <Input
-          name='title'
-          type='text'
-          defaultValue={project?.title}
-          register={register}
-          error={errors.title?.message}
-        />
-      </div>
-      <div>
-        <IoPerson size={20} className='text-geraldine' />
-        <Input
-          name='client'
-          type='text'
-          defaultValue={project?.client}
-          register={register}
-          error={errors.client?.message}
-        />
-      </div>
-      <div>
-        <IoIosColorPalette size={20} className='text-geraldine' />
-        <Input
-          name='color'
-          type='text'
-          defaultValue={project?.color.toLowerCase()}
-          register={register}
-          error={errors.color?.message}
-        />
-      </div>
-      <div>
-        <MdDateRange size={20} className='text-geraldine' />
-        <Input
-          name='date'
-          type='text'
-          defaultValue={project?.date}
-          register={register}
-          error={errors.date?.message}
-        />
-      </div>
-      <div>
-        <IoIosPricetags size={20} className='text-geraldine' />
-        <Input
-          name='price'
-          type='text'
-          defaultValue={project?.price}
-          register={register}
-          error={errors.price?.message}
-        />
-      </div>
-      <div>
-        <MdPhotoSizeSelectLarge size={20} className='text-geraldine' />
-        <Input
-          name='size'
-          type='text'
-          defaultValue={project?.size}
-          register={register}
-          error={errors.size?.message}
-        />
-      </div>
-      <div className='w-full'>
-        <MdDescription size={20} className='text-geraldine' />
+      <div className='w-full flex flex-col md:flex-row gap-2 md:gap-12 [&>*]:w-full [&>*]:center [&>*]:flex-col [&>*]:gap-2'>
+        <div className='[&>*]:w-full [&>*]:flex [&>*]:items-start [&>*]:gap-2 [&>*]:md:h-16'>
+          <label>
+            <IoSquare size={20} className='text-geraldine mt-2' />
+            <Input
+              name='title'
+              type='text'
+              defaultValue={project?.title}
+              register={register}
+              error={errors.title?.message}
+            />
+          </label>
+          <label>
+            <IoPerson size={20} className='text-geraldine mt-2' />
+            <Input
+              name='client'
+              type='text'
+              defaultValue={project?.client}
+              register={register}
+              error={errors.client?.message}
+            />
+          </label>
+          <label>
+            <IoIosColorPalette size={20} className='text-geraldine mt-2' />
+            <Input
+              name='color'
+              type='text'
+              defaultValue={project?.color.toLowerCase()}
+              register={register}
+              error={errors.color?.message}
+            />
+          </label>
+        </div>
 
-        <textarea
-          className='w-full p-2 shadow rounded-sm'
-          {...register('description')}
-          name='description'
-          defaultValue={project?.description}
-        />
-        <p className='my-1 text-red text-sm'>{errors.description?.message}</p>
+        <div className='[&>*]:w-full [&>*]:flex [&>*]:items-start [&>*]:gap-2 [&>*]:md:h-16'>
+          <label>
+            <MdDateRange size={20} className='text-geraldine mt-2' />
+            <Input
+              name='date'
+              type='text'
+              defaultValue={project?.date}
+              register={register}
+              error={errors.date?.message}
+            />
+          </label>
+          <label>
+            <IoIosPricetags size={20} className='text-geraldine mt-2' />
+            <Input
+              name='price'
+              type='text'
+              defaultValue={project?.price}
+              register={register}
+              error={errors.price?.message}
+            />
+          </label>
+          <label>
+            <MdPhotoSizeSelectLarge size={20} className='text-geraldine mt-2' />
+            <Input
+              name='size'
+              type='text'
+              defaultValue={project?.size}
+              register={register}
+              error={errors.size?.message}
+            />
+          </label>
+        </div>
       </div>
 
-      <div className='w-full flex justify-between'>
+      <div className='w-full min-h-24 flex items-start gap-2'>
+        <MdDescription size={20} className='text-geraldine mt-4' />
+        <div className='w-full'>
+          <textarea
+            className='w-full p-2 shadow shadow-geraldine bg-white outline-geraldine focus:outline-blue focus:shadow-blue'
+            {...register('description')}
+            name='description'
+            defaultValue={project?.description}
+          />
+          <p className='my-1 text-red text-sm'>{errors.description?.message}</p>
+        </div>
+      </div>
+
+      <div className='w-full mt-2 flex justify-between'>
         <button
           type='submit'
           className='w-28 h-10 shadow-md hover:shadow-none hover:scale-95 transition-all bg-geraldine hover:text-black'
