f41b5f0e451fb6923952157ba34b2db65889096c
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-22 15:59:17 -0300

    refactor: delete div and border radius

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -25,33 +25,26 @@ export function Input({
   }, []);
 
   return (
-    <div className='flex flex-col'>
-      <label className='pt-2'>
+    <div className='w-full flex flex-col'>
+      <label className='group flex shadow shadow-geraldine  focus-within:shadow-blue focus-within:outline outline-blue outline-1  bg-white [&>*]:bg-white'>
         {label}
-        <div
-          dir='ltr'
-          className='group flex rounded-s-lg rounded-e-lg shadow shadow-geraldine  focus-within:shadow-blue focus-within:outline outline-blue outline-1  bg-white [&>*]:bg-white'
-        >
-          <input
-            placeholder={placeholder}
-            defaultValue={defaultValue}
-            type={inputType}
-            {...register(name, rules)}
-            id={name}
-            className={`w-full h-10 p-2 rounded-s-lg outline-none ${
-              !isPassword && 'rounded-e-lg'
-            }`}
-          />
-          {isPassword && (
-            <button
-              type='button'
-              className='p-2 outline-none rounded-e-lg'
-              onClick={handleInputType}
-            >
-              {inputType === 'password' ? <IoEye /> : <IoEyeOff />}
-            </button>
-          )}
-        </div>
+        <input
+          placeholder={placeholder}
+          defaultValue={defaultValue}
+          type={inputType}
+          {...register(name, rules)}
+          id={name}
+          className='w-full h-10 p-2 outline-none'
+        />
+        {isPassword && (
+          <button
+            type='button'
+            className='p-2 outline-none'
+            onClick={handleInputType}
+          >
+            {inputType === 'password' ? <IoEye /> : <IoEyeOff />}
+          </button>
+        )}
       </label>
 
       {error && <p className='my-1 text-red text-sm'>{error}</p>}
