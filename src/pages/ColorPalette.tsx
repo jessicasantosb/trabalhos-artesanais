c305a98c69c66013889cb14b00e3732304cf234d
Jessica <jessica.santosb@outlook.com>
2024-12-23 10:39:49 -0300

    fix: add th to table

--- src/pages/ColorPalette.tsx
+++ src/pages/ColorPalette.tsx
@@ -11,27 +11,28 @@ export function ColorPalette() {
         subtitle='confira algumas paletas de cores que podem te inspirar na sua escolha'
       />
 
-      {palettes.map(({ title, palette }) => {
-        return (
-          <table key={title} className='w-full max-w-xl mb-6'>
-            <thead>
-              <tr className='text-2xl text-geraldine'>{title}</tr>
-            </thead>
+      {palettes.map(({ title, palette }) => (
+        <table key={title} className='w-full max-w-xl mb-6'>
+          <thead>
+            <tr className='text-2xl text-left text-geraldine'>
+              <th>{title}</th>
+            </tr>
+          </thead>
 
-            {palette.map(({ name, color }) => (
-              <tbody key={name}>
-                <tr className='border'>
-                  <td className='p-2'>{name}</td>
-                  <td className='text-right pr-2 text-xs'>{color}</td>
-                  <td
-                    style={{ backgroundColor: color }}
-                    className='text-right p-2 w-24'></td>
-                </tr>
-              </tbody>
-            ))}
-          </table>
-        );
-      })}
+          {palette.map(({ name, color }) => (
+            <tbody key={name}>
+              <tr className='border'>
+                <td className='p-2'>{name}</td>
+                <td className='text-right pr-2 text-xs'>{color}</td>
+                <td
+                  style={{ backgroundColor: color }}
+                  className='text-right p-2 w-24'
+                />
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      ))}
     </section>
   );
 }
